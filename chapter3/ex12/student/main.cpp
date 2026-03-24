536b3238350b1df69711e178c23624deefcf7b8e
IsaiahSchaferV7 <schoolschafer@gmail.com>
2026-03-24 03:40:21 +0000

    chapter3/ex12 Task1

--- chapter3/ex12/student/main.cpp
+++ chapter3/ex12/student/main.cpp
@@ -1,11 +1,15 @@
 #include <iostream>
 #include <iomanip>
+#include <cmath>
 using namespace std;
 
 int main() {
     float speedOne;
     float speedTwo;
     float timeX, timeY;
+    float distance1;
+    float distance2;
+    float totalDistance;
 
     cout << fixed << setprecision(2);
 
@@ -20,9 +24,12 @@ int main() {
         timeY = timeY / 60;
         timeX = timeX + timeY;
     }
+    
+    distance1 = timeX * speedOne;
+    distance2 = timeX * speedTwo;
+    totalDistance = sqrt((distance1 * distance1) + (distance2 * distance2));
 
-    cout << "The distance between the cars is " <<
-    (timeX * speedOne) + (timeX * speedTwo) << " miles" << endl;
+    cout << "The distance between the cars is " << totalDistance << " miles" << endl;
 
     return 0;
 }
