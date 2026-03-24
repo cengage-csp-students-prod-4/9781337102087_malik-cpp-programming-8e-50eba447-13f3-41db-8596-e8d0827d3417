929d02cb2b2211c1dab834cd3660f9b31f58b8f7
IsaiahSchaferV7 <schoolschafer@gmail.com>
2026-03-24 03:27:04 +0000

    chapter2/ex18 Task1

--- chapter2/ex18/student/main.cpp
+++ chapter2/ex18/student/main.cpp
@@ -4,7 +4,12 @@ using namespace std;
 
 int main() {
     float payRate;
-    float hoursWorked;
+    float hoursWorked1;
+    float hoursWorked2;
+    float hoursWorked3;
+    float hoursWorked4;
+    float hoursWorked5;
+    float totalHoursWorked;
     const float taxRate = 0.14;
     const float clothesSupR = 0.10;
     const float schoolSupR = 0.01;
@@ -18,10 +23,19 @@ int main() {
 
     cout << "Enter your payrate and press Enter: ";
     cin >> payRate;
-    cout << "Enter number of hours worked and press Enter ";
-    cin >> hoursWorked;
+    cout << "Enter number of hours worked week 1 and press Enter ";
+    cin >> hoursWorked1;
+    cout << "Enter number of hours worked week 2 and press Enter ";
+    cin >> hoursWorked2;
+    cout << "Enter number of hours worked week 3 and press Enter ";
+    cin >> hoursWorked3;
+    cout << "Enter number of hours worked week 4 and press Enter ";
+    cin >> hoursWorked4;
+    cout << "Enter number of hours worked week 5 and press Enter ";
+    cin >> hoursWorked5;
 
-    payTotal = payRate * hoursWorked;
+    totalHoursWorked = hoursWorked1 + hoursWorked2 + hoursWorked3 + hoursWorked4 + hoursWorked5;
+    payTotal = payRate * totalHoursWorked;
     taxedTotal = payTotal - (payTotal * taxRate);
     clothesSup = clothesSupR * taxedTotal;
     schoolSup = schoolSupR * taxedTotal;
