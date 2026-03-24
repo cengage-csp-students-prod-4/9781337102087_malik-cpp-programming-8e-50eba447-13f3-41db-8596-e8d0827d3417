35e4a47a05f0373ee91efe242767de744221d827
IsaiahSchaferV7 <schoolschafer@gmail.com>
2026-03-24 03:19:20 +0000

    chapter3/ex12 Task1

--- chapter3/ex12/student/main.cpp
+++ chapter3/ex12/student/main.cpp
@@ -1,8 +1,28 @@
 #include <iostream>
-
+#include <iomanip>
 using namespace std;
 
 int main() {
-    // Write your main here
+    float speedOne;
+    float speedTwo;
+    float timeX, timeY;
+
+    cout << fixed << setprecision(2);
+
+    cout << "Enter the average speed of the first car: ";
+    cin >> speedOne;
+    cout << "Enter the average speed of the second car: ";
+    cin >> speedTwo;
+    cout << "Enter the elapsed time (hours than minutes) since the cars left: ";
+    cin >> timeX >> timeY;
+
+    if (timeY > 0) {
+        timeY = timeY / 60;
+        timeX = timeX + timeY;
+    }
+
+    cout << "The distance between the cars is " <<
+    (timeX * speedOne) + (timeX * speedTwo) << " miles" << endl;
+
     return 0;
 }
