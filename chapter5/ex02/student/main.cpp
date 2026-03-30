62ba9cacac43158113e7a74ad8690ed1c53b7d96
IsaiahSchaferV7 <schoolschafer@gmail.com>
2026-03-30 23:58:10 +0000

    chapter5/ex02 Task1

--- chapter5/ex02/student/main.cpp
+++ chapter5/ex02/student/main.cpp
@@ -16,9 +16,9 @@ int main()
     for (i = 0; i < n; i++)
     {
         if (i % 2 == 0)
-            pi = pi - (1.0 / (2 * i + 1));
-        else
             pi = pi + (1.0 / (2 * i + 1));
+        else
+            pi = pi - (1.0 / (2 * i + 1));
     }
     
     pi = 4 * pi;
