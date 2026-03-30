a00991ff98f8eb5afb5e5d7d641fdbe47cb425af
IsaiahSchaferV7 <schoolschafer@gmail.com>
2026-03-30 23:53:03 +0000

    chapter5/ex02 Task1

--- chapter5/ex02/student/main.cpp
+++ chapter5/ex02/student/main.cpp
@@ -6,7 +6,7 @@ using namespace std;
 int main()
 {
     double pi = 0;
-    long i;
+    double long i;
     long n;
     
     cin >> n;
@@ -16,11 +16,12 @@ int main()
     for (i = 0; i < n; i++)
     {
         if (i % 2 == 0)
-            pi = pi - (1 / (2 * i + 1));
+            pi = pi - (1.0 / (2 * i + 1));
         else
-            pi = pi + (1 / (2 * i + 1));
-        pi = 4 * pi;
+            pi = pi + (1.0 / (2 * i + 1));
     }
+    
+    pi = 4 * pi;
     
     cout << endl << "pi = " << pi << endl;
     return 0;
